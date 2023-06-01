600ff144b3810ed4f199dc0aafce1f1f8fa10130
iozefavichus <iozefavichus@gmail.com>
2023-06-01 20:32:07 +0300

    feat: add solutions for conditions and loops tasks

--- src/07-conditions-n-loops-tasks.js
+++ src/07-conditions-n-loops-tasks.js
@@ -139,8 +139,12 @@ function isTriangle(a, b, c) {
  *   { top:20, left:20, width: 20, height: 20 }    =>  false
  *
  */
-function doRectanglesOverlap(/* rect1, rect2 */) {
-  throw new Error('Not implemented');
+function doRectanglesOverlap(rect1, rect2) {
+  let result = false;
+  if (rect2.top <= rect1.top + rect1.width && rect2.left < rect1.left + rect1.height) {
+    result = true;
+  }
+  return result;
 }
 
 
@@ -170,8 +174,14 @@ function doRectanglesOverlap(/* rect1, rect2 */) {
  *   { center: { x:0, y:0 }, radius:10 },  { x:10, y:10 }   => false
  *
  */
-function isInsideCircle(/* circle, point */) {
-  throw new Error('Not implemented');
+function isInsideCircle(circle, point) {
+  let result = false;
+  const a = point.x - circle.center.x;
+  const c = point.y - circle.center.y;
+  if (a * a + c * c < circle.radius * circle.radius) {
+    result = true;
+  }
+  return result;
 }
 
 
@@ -186,8 +196,18 @@ function isInsideCircle(/* circle, point */) {
  *   'abracadabra'  => 'c'
  *   'entente' => null
  */
-function findFirstSingleChar(/* str */) {
-  throw new Error('Not implemented');
+function findFirstSingleChar(str) {
+  let result = null;
+  const arr = str.split('');
+  for (let i = 0; i < arr.length; i += 1) {
+    if (result) {
+      break;
+    }
+    if (!((arr.includes(arr[i], i + 1) || (arr.slice(0, i).includes(arr[i]))))) {
+      result = arr[i];
+    }
+  }
+  return result;
 }
 
 
